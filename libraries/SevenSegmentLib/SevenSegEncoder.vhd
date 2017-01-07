8db4c48c9cf4113a15c0148982ed3c9db86db3bc
Tim Prince <tim.prince@gmail.com>
2017-01-07 15:12:37 -0800

    Fix reversed seven segment mapping.

--- libraries/SevenSegmentLib/SevenSegEncoder.vhd
+++ libraries/SevenSegmentLib/SevenSegEncoder.vhd
@@ -17,22 +17,22 @@ begin
       segments <= (others => '0');
     else
       case Value is
-        when  0 => segments <= DpEnable & "1111110";
-        when  1 => segments <= DpEnable & "0110000";
-        when  2 => segments <= DpEnable & "1101101";
-        when  3 => segments <= DpEnable & "1111001";
-        when  4 => segments <= DpEnable & "0110011";
-        when  5 => segments <= DpEnable & "1011011";
-        when  6 => segments <= DpEnable & "1011111";
-        when  7 => segments <= DpEnable & "1110000";
+        when  0 => segments <= DpEnable & "0111111";
+        when  1 => segments <= DpEnable & "0000110";
+        when  2 => segments <= DpEnable & "1011011";
+        when  3 => segments <= DpEnable & "1001111";
+        when  4 => segments <= DpEnable & "1100110";
+        when  5 => segments <= DpEnable & "1101101";
+        when  6 => segments <= DpEnable & "1111101";
+        when  7 => segments <= DpEnable & "0000111";
         when  8 => segments <= DpEnable & "1111111";
-        when  9 => segments <= DpEnable & "1111011";
+        when  9 => segments <= DpEnable & "1101111";
         when 10 => segments <= DpEnable & "1110111";
-        when 11 => segments <= DpEnable & "0011111";
-        when 12 => segments <= DpEnable & "1001110";
-        when 13 => segments <= DpEnable & "0111101";
-        when 14 => segments <= DpEnable & "1001111";
-        when 15 => segments <= DpEnable & "1000111";
+        when 11 => segments <= DpEnable & "1111100";
+        when 12 => segments <= DpEnable & "0111001";
+        when 13 => segments <= DpEnable & "1011110";
+        when 14 => segments <= DpEnable & "1111001";
+        when 15 => segments <= DpEnable & "1110001";
       end case;
     end if;
   end process;
